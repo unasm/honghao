c2d714710ef4dd07c795914073630d1b71d368be
1264310280@qq.com <1264310280@qq.com@a1f260ce-6a73-4ba0-bf89-cd83dba2fba4>
2014-11-30 07:09:14 +0000

    fix bug
    git-svn-id: https://svn.sinaapp.com/honghaotouzi@111 a1f260ce-6a73-4ba0-bf89-cd83dba2fba4

--- 1/controller/home.php
+++ 1/controller/home.php
@@ -41,7 +41,11 @@ class Home extends Honghao
 			if(count($data) === 2){
 				$_GET['code'] = trim($data[0]);
 				$_GET['time'] = trim($data[1]);
-				$this->output->formStr(strlen($_GET['code']) . '@5', $res);
+				if(preg_match('/^\d+$/' , $$_GET['code'])){
+					$this->output->formStr('yes', $res);
+				} else{
+					$this->output->formStr('no', $res);
+				}
 				if(!$this->validate->check($_GET['code'] , 'int' , 7)){
 					$this->output->formStr($this->config['help'] . '1', $res);
 					$error = 1;
