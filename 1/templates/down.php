fc5df8f413811c0b5a8687ef1504a438c3ad0d17
1264310280@qq.com <1264310280@qq.com@a1f260ce-6a73-4ba0-bf89-cd83dba2fba4>
2014-12-05 17:11:22 +0000

    change display
    git-svn-id: https://svn.sinaapp.com/honghaotouzi@136 a1f260ce-6a73-4ba0-bf89-cd83dba2fba4

--- 1/templates/down.php
+++ 1/templates/down.php
@@ -6,18 +6,36 @@
 	
 </head>
 <body>
-	<p>请点击右上方或者右下方的按钮，然后选择在浏览器中打开</p>
-	<?php foreach($output as $data):?>
-	<div style = "width:100%">		
-		<p>披露时间:<?php  echo $data['time']?></p>
-		<p>股票代码:<?php  echo $data['code']?></p>
-		<a href = "<?php echo $data['link']?>"><h3><?php echo $data['title']?></h3></a>
-		<a href = "<?php echo $data['link']?>">
-			<button style  = "padding:5px">
-				点击下载
-			</button>
-		</a>
+	<div id = "float">
+		<p>请点击右上方或者右下方的按钮，然后选择在浏览器中打开</p>
 	</div>
-	<?php endforeach?>
+	<div id = "container">
+		<?php foreach($output as $data):?>
+		<div style = "width:100%">		
+			<p>披露时间:<?php  echo $data['time']?></p>
+			<p>股票代码:<?php  echo $data['code']?></p>
+			<a href = "<?php echo $data['link']?>"><h3><?php echo $data['title']?></h3></a>
+			<a href = "<?php echo $data['link']?>">
+				<button style  = "padding:5px">
+					点击下载
+				</button>
+			</a>
+		</div>
+		<?php endforeach?>
+	</div>
 </body>
+<style type="text/css" media="screen">
+	#float{
+		background:rgba(0,0,0,0.6);	
+		width:100%;
+		color:rgb(73, 235, 73);
+		font-size:1.2em;
+		position:absolute;
+		top:0;
+		left:0;
+	}
+	#container{
+		margin-top:40px;
+	}
+</style>
 </html>
